d3e20138cbccacabc6080e96a2b5833383772573
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-12-28 01:05:06 +0900

    Add board dto

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v4 as uuidv4 } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,7 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  createBoard({ title, description }: CreateBoardDto) {
     const board: Board = {
       id: uuidv4(), // 나중에 DB가 알아서 유니크 값을 줄거다.
       title,
