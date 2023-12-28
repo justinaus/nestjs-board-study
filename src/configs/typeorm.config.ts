6cd060d6ebfa32c86510715ef9416344dfe68d52
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-12-29 01:28:45 +0900

    Add pg, typeorm

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  // username: process.env.POSTGRES_USER,
+  // password: process.env.POSTGRES_PASSWORD,
+  // database: process.env.POSTGRES_DB,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'board-app',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+  logging: true,
+};
