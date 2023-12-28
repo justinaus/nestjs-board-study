58e0d557a1e789633d3efad96146b153d088a26c
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-12-29 02:05:08 +0900

    Update db 적용 전 주석 처리

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -11,39 +11,39 @@ import {
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
-import { BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
-  getAllBoards() {
-    return this.boardsService.getAllBoards();
-  }
+  // @Get()
+  // getAllBoards() {
+  //   return this.boardsService.getAllBoards();
+  // }
 
-  @Post()
-  @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto) {
-    return this.boardsService.createBoard(createBoardDto);
-  }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto) {
+  //   return this.boardsService.createBoard(createBoardDto);
+  // }
 
-  @Get('/:id')
-  getBoardById(@Param('id') id: string) {
-    return this.boardsService.getBoardById(id);
-  }
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string) {
+  //   return this.boardsService.getBoardById(id);
+  // }
 
-  @Delete('/:id')
-  deleteBoard(@Param('id') id: string) {
-    this.boardsService.deleteBoard(id);
-  }
+  // @Delete('/:id')
+  // deleteBoard(@Param('id') id: string) {
+  //   this.boardsService.deleteBoard(id);
+  // }
 
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ) {
-    return this.boardsService.updateBoardStatus(id, status);
-  }
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  // ) {
+  //   return this.boardsService.updateBoardStatus(id, status);
+  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,56 +1,42 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
-  private boards: Board[] = [];
-
-  getAllBoards() {
-    return this.boards;
-  }
-
-  createBoard({ title, description }: CreateBoardDto) {
-    const board: Board = {
-      id: uuidv4(), // 나중에 DB가 알아서 유니크 값을 줄거다.
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
-
-    this.boards.push(board);
-
-    return board;
-  }
-
-  // TODO. 왜 리턴 타입이 Board로 추론 되는지 이해가 안감. Board | undefined로 추론되어야 하는거 아닌가?
-  getBoardById(id: string) {
-    const found = this.boards.find((board) => board.id === id);
-
-    // 그냥 익셉션 알아서 처리해야되는듯.
-    if (!found) {
-      // throw new NotFoundException();
-
-      // 문구 커스텀.
-      throw new NotFoundException(`Can't find Board with id ${id}`);
-    }
-
-    return found;
-  }
-
-  deleteBoard(id: string) {
-    const found = this.getBoardById(id);
-
-    this.boards = this.boards.filter((board) => board.id !== found.id);
-  }
-
-  updateBoardStatus(id: string, status: BoardStatus) {
-    const board = this.getBoardById(id);
-
-    // TODO. undefined 처리?
-
-    board.status = status;
-    return board;
-  }
+  // getAllBoards() {
+  //   return this.boards;
+  // }
+  // createBoard({ title, description }: CreateBoardDto) {
+  //   const board: Board = {
+  //     id: uuidv4(), // 나중에 DB가 알아서 유니크 값을 줄거다.
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
+  //   this.boards.push(board);
+  //   return board;
+  // }
+  // // TODO. 왜 리턴 타입이 Board로 추론 되는지 이해가 안감. Board | undefined로 추론되어야 하는거 아닌가?
+  // getBoardById(id: string) {
+  //   const found = this.boards.find((board) => board.id === id);
+  //   // 그냥 익셉션 알아서 처리해야되는듯.
+  //   if (!found) {
+  //     // throw new NotFoundException();
+  //     // 문구 커스텀.
+  //     throw new NotFoundException(`Can't find Board with id ${id}`);
+  //   }
+  //   return found;
+  // }
+  // deleteBoard(id: string) {
+  //   const found = this.getBoardById(id);
+  //   this.boards = this.boards.filter((board) => board.id !== found.id);
+  // }
+  // updateBoardStatus(id: string, status: BoardStatus) {
+  //   const board = this.getBoardById(id);
+  //   // TODO. undefined 처리?
+  //   board.status = status;
+  //   return board;
+  // }
 }
