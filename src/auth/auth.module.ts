da943c6ed402ca8d074dcbd5bf9692fc8eaa9922
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2024-01-04 01:12:38 +0900

    Add jwt, passport - signin

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,22 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth.credential.dto';
 
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     // @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(dto: AuthCredentialDto) {
@@ -20,7 +22,11 @@ export class AuthService {
     const user = await this.userRepository.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // 유저 토큰 생성 ( Secret + Payload )
+      const payload = { username }; // payload에는 중요한 정보를 넣지 않는다.
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken: accessToken };
     } else {
       throw new UnauthorizedException('login failed');
     }
