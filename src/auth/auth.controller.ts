1d3cb999955e4b3e121cda002eda3683d7bbc5dd
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2024-01-12 00:00:06 +0900

    Update auth - test (guard)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth.credential.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +22,11 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body(ValidationPipe) dto: AuthCredentialDto) {
     return this.authService.signIn(dto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req) {
+    console.log('req', req);
   }
 }
